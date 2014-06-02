4b6c93b5a67d88a1127eb43d691e5dcf2f342d9c
Laser Nite <lasernite@gmail.com>
2014-06-02 15:33:44 -0500

    adding most of app.js to web.js

--- web.js
+++ web.js
@@ -3,7 +3,37 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 app.use(logfmt.requestLogger());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
