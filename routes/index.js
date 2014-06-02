0e749df6f76e311c229e226254ca55350ce81e7e
Laser Nite <lasernite@gmail.com>
2014-06-02 14:36:35 -0500

    image and procfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'EnjoyBliss' });
+  res.render('index', { title: 'Enjoy Bliss' });
 });
 
 module.exports = router;
