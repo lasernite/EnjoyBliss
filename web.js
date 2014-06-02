163499f2b5be49a88b766521525063a691b3c2a4
Laser Nite <lasernite@gmail.com>
2014-06-02 15:28:38 -0500

    that didnt work

--- web.js
+++ web.js
@@ -5,14 +5,6 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('*', function(req, res, next) {
-  if (req.headers.host.slice(0, 3) != 'www') {
-    res.redirect('http://www.' + req.headers.host + req.url, 301);
-  } else {
-    next();
-  }
-});
-
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
